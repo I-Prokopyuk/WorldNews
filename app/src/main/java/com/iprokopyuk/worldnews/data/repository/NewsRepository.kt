818a3bcf52faffd1879d7054eb7c6d4073554f6c
I Prokopyuk <prok.pismo@gmail.com>
2021-03-22 22:40:58 +0600

    Initial commit

--- app/src/main/java/com/iprokopyuk/worldnews/data/repository/NewsRepository.kt
+++ app/src/main/java/com/iprokopyuk/worldnews/data/repository/NewsRepository.kt
@@ -30,8 +30,8 @@ class NewsRepository
             "description": "Rafael Nadal is officially OUT of the U.S. Open ... the tennis legend said Tuesday it's just too damn unsafe for him to travel to America during the COVID-19 pandemic. \"The situation is very complicated worldwide,\" Nadal wrote in a statement. \"The…",
             "url": "https://www.tmz.com/2020/08/04/rafael-nadal-us-open-tennis-covid-19-concerns/",
             "source": "TMZ.com",
-            "image": null,
-            "category": "general",
+            "image": "https://imagez.tmz.com/image/fa/4by3/2020/08/04/fad55ee236fc4033ba324e941bb8c8b7_md.jpg",
+            "category": "sports",
             "language": "en",
             "country": "us",
             "published_at": "2020-08-05T05:47:24+00:00"
@@ -54,7 +54,7 @@ class NewsRepository
             "url": "https://www.tmz.com/2020/08/04/rafael-nadal-us-open-tennis-covid-19-concerns/",
             "source": "TMZ.com",
             "image": "https://imagez.tmz.com/image/fa/4by3/2020/08/04/fad55ee236fc4033ba324e941bb8c8b7_md.jpg",
-            "category": "science",
+            "category": "sports",
             "language": "en",
             "country": "us",
             "published_at": null
@@ -73,10 +73,8 @@ class NewsRepository
 //                )
 //            })
 
-
         callbackResult.onResultCallback(result)
     }
-
 
     fun saveToLocalDB(news: List<News>) {
 
@@ -88,51 +86,3 @@ class NewsRepository
 
 
 }
-
-
-//val jsonString =
-//    """{
-//    "pagination": {
-//        "limit": 100,
-//        "offset": 0,
-//        "count": 100,
-//        "total": 293
-//    },
-//    "data": [
-//        {
-//            "author": "TMZ Staff_111",
-//            "title": "Rafael Nadal Pulls Out Of U.S. Open Over COVID-19 Concerns",
-//            "description": "Rafael Nadal is officially OUT of the U.S. Open ... the tennis legend said Tuesday it's just too damn unsafe for him to travel to America during the COVID-19 pandemic. \"The situation is very complicated worldwide,\" Nadal wrote in a statement. \"The…",
-//            "url": "https://www.tmz.com/2020/08/04/rafael-nadal-us-open-tennis-covid-19-concerns/",
-//            "source": "TMZ.com",
-//            "image": "https://imagez.tmz.com/image/fa/4by3/2020/08/04/fad55ee236fc4033ba324e941bb8c8b7_md.jpg",
-//            "category": "general",
-//            "language": "en",
-//            "country": "us",
-//            "published_at": "2020-08-05T05:47:24+00:00"
-//        },
-//        {
-//            "author": "TMZ Staff_222",
-//            "title": "Rafael Nadal Pulls Out Of U.S. Open Over COVID-19 Concerns",
-//            "description": "Rafael Nadal is officially OUT of the U.S. Open ... the tennis legend said Tuesday it's just too damn unsafe for him to travel to America during the COVID-19 pandemic. \"The situation is very complicated worldwide,\" Nadal wrote in a statement. \"The…",
-//            "url": "https://www.tmz.com/2020/08/04/rafael-nadal-us-open-tennis-covid-19-concerns/",
-//            "source": "TMZ.com",
-//            "image": "https://imagez.tmz.com/image/fa/4by3/2020/08/04/fad55ee236fc4033ba324e941bb8c8b7_md.jpg",
-//            "category": "general",
-//            "language": "en",
-//            "country": "us",
-//            "published_at": "2020-08-05T05:47:24+00:00"
-//        },{
-//            "author": "TMZ Staff_333",
-//            "title": "Rafael Nadal Pulls Out Of U.S. Open Over COVID-19 Concerns",
-//            "description": "Rafael Nadal is officially OUT of the U.S. Open ... the tennis legend said Tuesday it's just too damn unsafe for him to travel to America during the COVID-19 pandemic. \"The situation is very complicated worldwide,\" Nadal wrote in a statement. \"The…",
-//            "url": "https://www.tmz.com/2020/08/04/rafael-nadal-us-open-tennis-covid-19-concerns/",
-//            "source": "TMZ.com",
-//            "image": "https://imagez.tmz.com/image/fa/4by3/2020/08/04/fad55ee236fc4033ba324e941bb8c8b7_md.jpg",
-//            "category": "general",
-//            "language": "en",
-//            "country": "us",
-//            "published_at": "2020-08-05T05:47:24+00:00"
-//        }
-//    ]
-//}"""
