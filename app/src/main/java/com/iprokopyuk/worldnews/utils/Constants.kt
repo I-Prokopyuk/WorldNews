61d9e3a0202e20e8ee9bcaf2b05c06ca2565691a
I Prokopyuk <prok.pismo@gmail.com>
2021-04-12 22:22:49 +0600

    Initial commit

--- app/src/main/java/com/iprokopyuk/worldnews/utils/Constants.kt
+++ app/src/main/java/com/iprokopyuk/worldnews/utils/Constants.kt
@@ -2,7 +2,8 @@ package com.iprokopyuk.worldnews.utils
 
 //API
 internal const val API_BASE_URL = "http://api.mediastack.com"
-internal const val API_KEY = "a39c5d527c17e3385bd2b992abb0e238"
+//internal const val API_KEY = "a81e429dbfc179bebba77fd6fc20cb12"
+internal const val API_KEY = "4fdf7f7722cdb0256f5ed9645df8f95c"
 
 //DB
 internal const val DB_VERSION = 1;
